8d1fd0be5ff2d6222bddfea6a9d4dd4a3dfc8550
nahidraz23 <nzamanraz@gmail.com>
2024-08-17 03:12:02 +0600

    watches count get api implemented

--- index.js
+++ index.js
@@ -1,55 +1,61 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-require('dotenv').config();
-const port = process.env.PORT || 5300;
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
+const express = require('express')
+const cors = require('cors')
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT || 5300
+const { MongoClient, ServerApiVersion } = require('mongodb')
 
 // middlewares
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 // MongoDB stetup
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xkofs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xkofs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
-    deprecationErrors: true,
+    deprecationErrors: true
   }
-});
+})
 
-async function run() {
+async function run () {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
 
-    const watchesCollection = client.db('eclipsewristDB').collection('watchesCollection');
+    const watchesCollection = client
+      .db('eclipsewristDB')
+      .collection('watchesCollection')
 
     app.get('/watches', async (req, res) => {
-        const result = await watchesCollection.find().toArray();    
-        res.send(result);
+      const result = await watchesCollection.find().toArray()
+      res.send(result)
     })
 
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    app.get('/watchesCount', async (req, res) => {
+        const count = await watchesCollection.estimatedDocumentCount();
+        res.send({count})
+    })
+
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    )
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
-run().catch(console.dir);
-
-
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
-    res.send('EclipseWrist server is running');
+  res.send('EclipseWrist server is running')
 })
 
 app.listen(port, () => {
-    console.log(`EclipseWrist server is running on port ${port}`);
+  console.log(`EclipseWrist server is running on port ${port}`)
 })
