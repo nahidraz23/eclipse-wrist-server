328514c47431e8454ce67a64f493eba61d4fa02a
nahidraz23 <nzamanraz@gmail.com>
2024-08-17 00:54:33 +0600

    get api implemented

--- index.js
+++ index.js
@@ -29,11 +29,17 @@ async function run() {
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
 
-    
+    const watchesCollection = client.db('eclipsewristDB').collection('watchesCollection');
+
+    app.get('/watches', async (req, res) => {
+        const result = await watchesCollection.find().toArray();    
+        res.send(result);
+    })
+
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
